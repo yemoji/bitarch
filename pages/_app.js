eed44e7347bf7a77b6328f4d2faa9a4057945e70
Hamed Kalantari <hamedkalantari@icloud.com>
2024-09-16 17:47:25 +0330

    addgoogle analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+import { GoogleAnalytics } from '@next/third-parties/google';
 import "../styles/globals.css";
 import Layout from "../components/layout";
+
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-N5GYNNEELK" />
     </Layout>
   );
 }
